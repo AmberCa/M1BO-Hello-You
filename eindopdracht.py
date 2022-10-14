dbb77ea5cef6c8495f5bbd4b52661cb00a4c03dd
Amber <34961@ma-web.nl>
2022-10-14 10:57:01 +0200

    deel 1 code intro begin vraag1

--- eindopdracht.py
+++ eindopdracht.py
@@ -1,1 +1,24 @@
 "# M1BO-Hello-You" 
+
+
+#intro oorlog
+def introoorlog():
+    print ("Hello there...")
+    print ("This is weird, you here...")
+    print ("There is a war going on...")
+    
+    #vraaga
+    print ("You can a: Hide  b: Walk around  c: Run around")
+    anta = input()
+    if anta = "a":
+        print ("You fount a hut...")
+        print ("In your hut is no food and water")
+        print ("You ")
+    if anta = "b":
+        print (anta)
+
+    if anta = "c":
+        print ("")
+
+
+introoorlog()
